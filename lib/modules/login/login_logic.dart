84828f804b21e7d11ae749d27bf71d5c517553d7
joao thimotheo <jpthimotheo@hotmail.com>
2022-06-10 01:32:20 -0300

    feat: equipes adicionadas e adicionadas e implementadas snackbar de alerta

--- lib/modules/login/login_logic.dart
+++ lib/modules/login/login_logic.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:calculadora_utp/modules/login/login_state.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class LoginLogic extends GetxController {
@@ -12,23 +13,65 @@ class LoginLogic extends GetxController {
     switch (state.groupController.text) {
       case "15":
         state.groupName.value = "Jaguara";
-        log("Bem vindos ${state.groupName.value}");
+        state.members.addAll(['Camylla', 'Rodrigo']);
+        Get.snackbar(
+          'Bem Vindos',
+          'Equipe ${state.groupName.value}',
+          backgroundColor: Colors.green,
+          snackPosition: SnackPosition.BOTTOM,
+        );
         break;
       case "7":
         state.groupName.value = "Inverga mais não quebra";
+        state.members.addAll(['Fabbio', 'Lucas', 'Marcelo']);
+        Get.snackbar(
+          'Bem Vindos',
+          'Equipe ${state.groupName.value}',
+          backgroundColor: Colors.green,
+          snackPosition: SnackPosition.BOTTOM,
+        );
         break;
       case "9":
         state.groupName.value = "Ladeira abaixo";
+        state.members.addAll(['Eduardo', 'Ricardo', 'Welington']);
+        Get.snackbar(
+          'Bem Vindos',
+          'Equipe ${state.groupName.value}',
+          backgroundColor: Colors.green,
+          snackPosition: SnackPosition.BOTTOM,
+        );
         break;
       case "125":
         state.groupName.value = "AWA";
+        state.members.addAll(['Airton', 'Alexandre', 'Willian']);
+        Get.snackbar(
+          'Bem Vindos',
+          'Equipe ${state.groupName.value}',
+          backgroundColor: Colors.green,
+          snackPosition: SnackPosition.BOTTOM,
+        );
         break;
       case "72":
         state.groupName.value = "Cavalo de Troia";
+        state.members.addAll(['Ghiorge', 'Henrique']);
+        Get.snackbar(
+          'Bem Vindos',
+          'Equipe ${state.groupName.value}',
+          backgroundColor: Colors.green,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+        );
         break;
       default:
         log("Não foi encontrada nenhuma equpe");
         state.groupName.value = "Equipe não encontrada";
+        Get.snackbar(
+          'Falha',
+          'Equipe não encontrada no banco de dados',
+          backgroundColor: Colors.red,
+          snackPosition: SnackPosition.BOTTOM,
+          colorText: Colors.white,
+        );
     }
   }
 }
--- lib/modules/login/login_state.dart
+++ lib/modules/login/login_state.dart
@@ -1,18 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-import '../../models/group_model.dart';
-
 class LoginState {
   late TextEditingController groupController;
   late RxInt groupId;
   late RxString groupName;
-  late Rxn<GroupModel> members;
+  late RxList<String> members;
 
   LoginState() {
     groupController = TextEditingController();
     groupId = RxInt(0);
     groupName = RxString('');
-    members = Rxn<GroupModel>();
+    members = RxList<String>();
   }
 }
